2793cc7920b60feac98e43a3b2b39d8d216941ed
oonray <alexanderbjornsrud@gmail.com>
2023-03-07 15:50:42 +0100

    .

--- auth/jwt/c_src/handlers.c
+++ auth/jwt/c_src/handlers.c
@@ -2,6 +2,7 @@
 #include "mongoose/mongoose.h"
 
 void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+error:
   def(c, ev, ev_data, fn_data);
 }
 void def(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
